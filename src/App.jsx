4c75aa5b0288e4959feca1c3d4157954d9ddee48
FAIZAN\faiza <faizanmehmood3165@gmail.com>
2023-11-27 01:04:36 +0500

    refactor initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import { Navbar } from "./components";
-import { Home } from "./pages";
+import { Navbar, Footer } from "./components";
+import { About, Contact, Home, Projects } from "./pages";
 
 const App = () => {
   return (
@@ -11,6 +11,23 @@ const App = () => {
 
         <Routes>
           <Route path="/" element={<Home />} />
+
+          <Route
+            path="/*"
+            element={
+              <>
+                <Routes>
+                  <Route path="/about" element={<About />} />
+
+                  <Route path="/projects" element={<Projects />} />
+
+                  <Route path="/contact" element={<Contact />} />
+                </Routes>
+
+                <Footer />
+              </>
+            }
+          />
         </Routes>
       </Router>
     </main>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,38 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="header">
-      <NavLink to={"/"}>
-        <p>FM</p>
+      <NavLink
+        to={"/"}
+        className={
+          "w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+        }
+      >
+        <p className="blue-gradient_text">FM</p>
       </NavLink>
-    </header>
-  )
-}
 
-export default Navbar
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to={"/about"}
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          About
+        </NavLink>
+
+        <NavLink
+          to={"/projects"}
+          className={({ isActive }) =>
+            isActive ? "text-blue-500" : "text-black"
+          }
+        >
+          Projects
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
