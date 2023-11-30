e2b86fef648953c19b1d84bf899f5f5d613335e8
FAIZAN\faiza <faizanmehmood3165@gmail.com>
2023-11-30 22:31:05 +0500

    update my projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -182,23 +182,23 @@ export const socialLinks = [
 ];
 
 export const projects = [
-  {
-    iconUrl: pricewise,
-    theme: "btn-back-red",
-    name: "Amazon Price Tracker",
-    description:
-      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
-    link: "https://github.com/adrianhajdin/pricewise",
-  },
+  // {
+  //   iconUrl: pricewise,
+  //   theme: "btn-back-red",
+  //   name: "Amazon Price Tracker",
+  //   description:
+  //     "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
+  //   link: "https://github.com/adrianhajdin/pricewise",
+  // },
 
-  {
-    iconUrl: threads,
-    theme: "btn-back-green",
-    name: "Full Stack Threads Clone",
-    description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: "https://github.com/adrianhajdin/threads",
-  },
+  // {
+  //   iconUrl: threads,
+  //   theme: "btn-back-green",
+  //   name: "Full Stack Threads Clone",
+  //   description:
+  //     'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+  //   link: "https://github.com/adrianhajdin/threads",
+  // },
 
   {
     iconUrl: car,
@@ -209,14 +209,14 @@ export const projects = [
     link: "https://github.com/adrianhajdin/project_next13_car_showcase",
   },
 
-  {
-    iconUrl: snapgram,
-    theme: "btn-back-pink",
-    name: "Full Stack Instagram Clone",
-    description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://github.com/adrianhajdin/social_media_app",
-  },
+  // {
+  //   iconUrl: snapgram,
+  //   theme: "btn-back-pink",
+  //   name: "Full Stack Instagram Clone",
+  //   description:
+  //     "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
+  //   link: "https://github.com/adrianhajdin/social_media_app",
+  // },
 
   {
     iconUrl: estate,
@@ -224,7 +224,7 @@ export const projects = [
     name: "Real-Estate Application",
     description:
       "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-    link: "https://github.com/adrianhajdin/projects_realestate",
+    link: "https://property-prowess.vercel.app/",
   },
 
   {
